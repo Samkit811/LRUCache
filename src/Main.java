c0ab73c7aea34abd4bc37a7c692bdab19412c91a
samkits <samkitsalot0@gmail.com>
2025-04-23 13:03:37 +0530

    Added example of the caching in the main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
+        DBConnection dbConnection = DBConnection.getInstance();
+
+        L1Cache l1Cache = new L1Cache(5, dbConnection);
+
+        for(int i = 0; i < 5; i++){
+            Integer value = l1Cache.search(i);
+            System.out.println("Getting value: " + value + " for the key: " + i);
+        }
+
+        System.out.println("---- After adding Element -----");
+
+        Integer value = l1Cache.search(0);
+        System.out.println("Getting value: " + value + " for the key: " + 0);
+
+        value = l1Cache.search(10);
+        System.out.println("Getting value: " + value + " for the key: " + 10);
     }
 }
